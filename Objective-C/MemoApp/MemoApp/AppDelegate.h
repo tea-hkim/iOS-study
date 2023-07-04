43967c8cd6e6582f9972c28a26e0469f76f6ced2
tea-hkim <cnt.taehokim@gmail.com>
2023-07-04 11:35:06 +0900

    Objective-C: 메모앱 프로젝트 생성

---
+++ Objective-C/MemoApp/MemoApp/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  MemoApp
+//
+//  Created by Nick on 2023/07/04.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Objective-C/MemoApp/MemoApp/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  MemoApp
+//
+//  Created by Nick on 2023/07/04.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
