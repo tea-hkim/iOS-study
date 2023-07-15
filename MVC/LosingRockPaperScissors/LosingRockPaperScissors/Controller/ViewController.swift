0babaaa696041fe4bfdf59449b28b3fa97d86e1d
Nick(taehoKim) <xogh8025@gmail.com>
2023-07-15 21:24:48 +0900

    MVC: 지는 가위바위보 프로젝트 세팅
    # <type>(<scope>) : <subject>
    # 제목은 50자 이내 / 변경사항이 "무엇"인지 명확히 작성 / 끝에 마침표 X
    # 타입과 제목은 필수 / 범위는 선택
    # 예) feat : 로그인 기능 추가
    
    # 바로 아래 공백 유지 (제목과 본문의 분리를 위함)
    
    ################
    # Body Message (선택사항)
    # 본문(구체적인 내용)을 아랫줄에 작성
    # 여러 줄의 메시지를 작성할 땐 "-"로 구분 (한 줄은 72자 이내)
    
    ################
    # 꼬릿말(footer)을 아랫줄에 작성 (현재 커밋과 관련된 이슈 번호 추가 등)
    # 예) Close #7
    
    # Issue Tracker Number or URL
    
    # --- COMMIT END ---
    # <type> list
    #Feat:sparkles:: 새로운 기능
    #Fix🐞: 버그 수정
    #Docs:books: 문서 수정
    #Design:art:: UI 디자인 변경
    #Resource:bento:: 리소스 추가
    #Refactor:recycle:: 코드 리팩토링
    #Chore:broom:: 자질구레한 수정사항
    #Setting:gear:: 패키지 추가 등 세팅 관련
    #HOTFIX:ambulance:: 치명적인 버그 수정
      
    # ------------------
    # Remember me ~
    #   Capitalize the subject line
    #     제목 첫 글자를 대문자로
    #   Use the imperative mood in the subject line
    #     제목은 명령문 사용 (과거형 X)
    #   Do not end the subject line with a period
    #     제목 끝에 마침표(.) 금지
    #   Separate subject from body with a blank line
    #     제목과 본문을 한 줄 띄워 분리하기 (빈 행으로 구분)
    #   Use the body to explain what and why vs. how
    #     본문은 "어떻게" 보다 "무엇을", "왜"를 설명한다.
    #   Can use multiple lines with "-" for bullet points in body
    #     본문에 여러줄의 메시지(목록)를 작성할 땐 "-"로 구분
    # ------------------

---
+++ MVC/LosingRockPaperScissors/LosingRockPaperScissors/Controller/ViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ViewController.swift
+//  LosingRockPaperScissors
+//
+//  Created by 김태호 on 2023/07/14.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    
+    // MARK: - Property
+    
+    private var timer: Timer?
+    private var rpsNumber: Int = 0
+    private var userPick: Rps? = nil
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var computerRPS: UIImageView!
+    
+    @IBOutlet weak var rockButton: UIImageView!
+    @IBOutlet weak var paperButton: UIImageView!
+    @IBOutlet weak var scissorsButton: UIImageView!
+    @IBOutlet weak var startButton: UIButton!
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setAttribute()
+    }
+    
+    // MARK: - Function
+    
+    private func setAttribute() {
+        startButton.setTitleColor(.white, for: .normal)
+    }
+    
+    private func changeCompueterRPS() {
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) {[weak self] _ in
+            DispatchQueue.main.async {
+                guard let self else { return }
+                self.rpsNumber = (self.rpsNumber + 1) % 3
+                self.computerRPS.image = Rps.allCases[self.rpsNumber % 3].info.image
+            }
+        }
+    }
+    
+    private func startGame() {
+        changeCompueterRPS()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.timer?.invalidate()
+            self.timer = nil
+            self.startButton.isHidden = false
+        }
+    }
+    
+    private func getResult() {
+        
+    }
+    
+    private func getReady() {
+        titleLabel.text = "지는 가위바위보"
+        startButton.setTitle("시작", for: .normal)
+        resultLabel.isHidden = true
+        resultLabel.text = ""
+    }
+    
+    
+    // MARK: - IBAction
+    
+    @IBAction func startButtoTapped(_ sender: UIButton) {
+        startGame()
+        startButton.isHidden = true
+    }
+    
+
+}
+
