4551a22f9b33475a08b1ad529fecbba9d3900cda
tea-hkim <cnt.taehokim@gmail.com>
2023-08-04 15:10:14 +0900

    CollectionView: 공식문서 파헤치기 - TwoColumn Layout

--- CollectionView/ModernCollectionView/ModernCollectionView/Basic ViewController/TwoColumnViewController.swift
+++ CollectionView/ModernCollectionView/ModernCollectionView/Basic ViewController/TwoColumnViewController.swift
@@ -1,22 +1,23 @@
 //
-//  GridViewController.swift
+//  TwoColumnViewController.swift
 //  ModernCollectionView
 //
-//  Created by Nick on 2023/08/03.
+//  Created by Nick on 2023/08/04.
 //
 
 import UIKit
 
-final class GridViewController: UIViewController {
-
+class TwoColumnViewController: UIViewController {
+    
     // MARK: - Property
     
     private var collectionView: UICollectionView?
-    private var dataSource: UICollectionViewDiffableDataSource<Section, Int>! = nil
+    private var dataSource: UICollectionViewDiffableDataSource<Section, Int>? = nil
+    
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         configureHierachy()
         configureDataSource()
         makeSnapShot()
@@ -24,12 +25,12 @@ final class GridViewController: UIViewController {
     
 }
 
-// MARK: - Configure CollectionView
+// MARK: - Configure Layout
 
-extension GridViewController {
+extension TwoColumnViewController {
     
     enum Section {
-        case Grid
+        case Column
     }
     
     private func configureHierachy() {
@@ -39,43 +40,36 @@ extension GridViewController {
         view.addSubview(collectionView ?? UICollectionView())
     }
     
-    // UICollectionViewLayout을 리턴하고 있는데 UICollectionViewCompositionalLayout을 리턴해도 됨
     private func createLayout() -> UICollectionViewLayout {
-        let itemCount = 3
-        let groupCount = 4
-        let itemWitdthFraction: CGFloat = 1.0 / CGFloat(itemCount)
-        let groupHeightFraction: CGFloat = 1.0 / CGFloat(groupCount)
-        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(itemWitdthFraction), heightDimension: .fractionalHeight(1.0))
+        let groupColumnCount = 2
+        let groupWithFraction: CGFloat = 1.0 / CGFloat(groupColumnCount)
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        item.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 0)
         
-        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(groupHeightFraction))
-        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, repeatingSubitem: item, count: itemCount)
-        group.interItemSpacing = .fixed(10)
-        group.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10)
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(groupWithFraction), heightDimension: .absolute(60))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, repeatingSubitem: item, count: groupColumnCount)
         
         let section = NSCollectionLayoutSection(group: group)
-
         return UICollectionViewCompositionalLayout(section: section)
     }
-    
+
     private func configureDataSource() {
         let cellRegistration = UICollectionView.CellRegistration<TextCell, Int> { (cell, indexPath, identifier) in
             cell.configureComponent(with: "\(identifier)")
         }
         
         guard let collectionView else { return }
-        dataSource = UICollectionViewDiffableDataSource<Section, Int>(collectionView: collectionView) {
+        dataSource = UICollectionViewDiffableDataSource<Section, Int>(collectionView: collectionView, cellProvider: {
             (collectionView: UICollectionView, indexPath: IndexPath, identifier: Int) -> UICollectionViewCell? in
             return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: identifier)
-        }
+        })
     }
     
     private func makeSnapShot() {
         var snapshot = NSDiffableDataSourceSnapshot<Section, Int>()
-        snapshot.appendSections([.Grid])
-        snapshot.appendItems(Array(0..<94))
-        dataSource.apply(snapshot, animatingDifferences: false)
+        snapshot.appendSections([.Column])
+        snapshot.appendItems(Array(0..<100))
+        dataSource?.apply(snapshot, animatingDifferences: false)
     }
     
 }
