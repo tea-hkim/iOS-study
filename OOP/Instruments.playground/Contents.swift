54a81b676d3bd2429e7d4a693442d61b5fef3695
Nick(taehoKim) <xogh8025@gmail.com>
2023-08-13 15:11:36 +0900

    OOP: 다형성의 이해

--- OOP/Instruments.playground/Contents.swift
+++ OOP/Instruments.playground/Contents.swift
@@ -4,6 +4,8 @@ import UIKit
 /// Object Oriented Programming in Swift
 ///
 
+// MARK: - Class
+
 class Music {
     let notes: [String]
     
@@ -57,9 +59,9 @@ class Piano: Instrument {
     }
     
     override func play(_ music: Music) -> String {
-      return play(music, usingPedals: hasPedals)
+        return play(music, usingPedals: hasPedals)
     }
-
+    
     
     func play(_ music: Music, usingPedals: Bool) -> String {
         let preparedNotes = super.play(music)
@@ -72,4 +74,125 @@ class Piano: Instrument {
     
 }
 
+class Guitar: Instrument {
+    let stringGauge: String
+    
+    init(brand: String, stringGauge: String = "medium") {
+        self.stringGauge = stringGauge
+        super.init(brand: brand)
+    }
+}
 
+class AcousticGuitar: Guitar {
+    static let numberOfStrings = 6
+    static let fretCount = 20
+    
+    override func tune() -> String {
+        return "Tune \(brand) acoustic with E A D G B E"
+    }
+    
+    override func play(_ music: Music) -> String {
+        let preparedNotes = super.play(music)
+        return "Play folk tune on frets \(preparedNotes)."
+    }
+}
+
+class Amplifier {
+    private var _volume: Int
+    private(set) var isOn: Bool
+    
+    init() {
+        isOn = false
+        _volume = 0
+    }
+    
+    func plugIn() {
+        isOn = true
+    }
+    
+    func unplug() {
+        isOn = false
+    }
+    
+    var volume: Int {
+        get {
+            return isOn ? _volume : 0
+        } set {
+            _volume = min(max(newValue, 0), 10)
+        }
+    }
+}
+
+
+class ElectricGuitar: Guitar {
+    let amplifier: Amplifier
+    
+    init(brand: String, stringGauge: String = "light", amplifier: Amplifier) {
+        self.amplifier = amplifier
+        super.init(brand: brand, stringGauge: stringGauge)
+    }
+    
+    override func tune() -> String {
+        amplifier.plugIn()
+        amplifier.volume = 5
+        return "Tune \(brand) electric with E A D G B E"
+    }
+    
+    override func play(_ music: Music) -> String {
+        let preparedNotes = super.play(music)
+        return "Play solo \(preparedNotes) at volume \(amplifier.volume)."
+    }
+}
+
+class BassGuitar: Guitar {
+  let amplifier: Amplifier
+
+  init(brand: String, stringGauge: String = "heavy", amplifier: Amplifier) {
+    self.amplifier = amplifier
+    super.init(brand: brand, stringGauge: stringGauge)
+  }
+
+  override func tune() -> String {
+    amplifier.plugIn()
+    return "Tune \(brand) bass with E A D G"
+  }
+
+  override func play(_ music: Music) -> String {
+    let preparedNotes = super.play(music)
+    return "Play bass line \(preparedNotes) at volume \(amplifier.volume)."
+  }
+}
+
+class Band {
+  let instruments: [Instrument]
+  
+  init(instruments: [Instrument]) {
+    self.instruments = instruments
+  }
+  
+  func perform(_ music: Music) {
+      instruments.forEach { $0.perform(music) }
+  }
+}
+
+
+// MARK: - Instance
+
+let piano = Piano(brand: "Yamaha", hasPedals: true)
+piano.tune()
+
+let acousticGuitar = AcousticGuitar(brand: "Rorand", stringGauge: "medium")
+acousticGuitar.tune()
+
+
+let amplifier = Amplifier()
+let electricGuitar = ElectricGuitar(brand: "Gibson", stringGauge: "medium", amplifier: amplifier)
+electricGuitar.tune()
+
+let bassGuitar = BassGuitar(brand: "Fender", stringGauge: "heavy", amplifier: amplifier)
+bassGuitar.tune()
+
+let instruments = [piano, acousticGuitar, electricGuitar, bassGuitar]
+let band = Band(instruments: instruments)
+let music = Music(notes: ["C", "F", "G"])
+band.perform(music)
