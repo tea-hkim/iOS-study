4e7bff1f2e141ff3d44f12be573255e3a2a7091f
Nick(taeho Kim) <cnt.taehokim@gmail.com>
2023-11-26 19:00:51 +0900

    BasicTodoList init

---
+++ Basic TodoList/BasicTodoList/BasicTodoList/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  BasicTodoList
+//
+//  Created by 김태호 on 11/26/23.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
---
+++ Basic TodoList/BasicTodoList/BasicTodoListTests/BasicTodoListTests.swift
@@ -0,0 +1,19 @@
+//
+//  BasicTodoListTests.swift
+//  BasicTodoListTests
+//
+//  Created by 김태호 on 11/26/23.
+//
+
+import XCTest
+@testable import BasicTodoList
+
+final class BasicTodoListTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+}
