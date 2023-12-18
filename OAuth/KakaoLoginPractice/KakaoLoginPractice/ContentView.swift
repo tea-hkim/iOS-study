0af993a8f0704defc8bc797dfefdb1318883b3e3
Nick(taehoKim) <xogh8025@gmail.com>
2023-12-18 20:55:23 +0900

    feat: kakao login setting

---
+++ OAuth/KakaoLoginPractice/KakaoLoginPractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KakaoLoginPractice
+//
+//  Created by 김태호 on 12/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OAuth/KakaoLoginPractice/KakaoLoginPractice/KakaoLoginPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  KakaoLoginPracticeApp.swift
+//  KakaoLoginPractice
+//
+//  Created by 김태호 on 12/17/23.
+//
+
+import SwiftUI
+
+@main
+struct KakaoLoginPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
