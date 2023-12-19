d908e8b5aee36211583a0152c76412841f0e7b36
tea-hkim <cnt.taehokim@gmail.com>
2023-12-19 10:59:08 +0900

    Feat: set AppleLogin View

---
+++ OAuth/AppleLoginPractice/AppleLoginPractice/AppleLoginPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleLoginPracticeApp.swift
+//  AppleLoginPractice
+//
+//  Created by Nick on 2023/12/19.
+//
+
+import SwiftUI
+
+@main
+struct AppleLoginPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OAuth/AppleLoginPractice/AppleLoginPractice/SignInWithAppleView.swift
@@ -0,0 +1,8 @@
+//
+//  SignInWithAppleView.swift
+//  AppleLoginPractice
+//
+//  Created by Nick on 2023/12/19.
+//
+
+import Foundation
